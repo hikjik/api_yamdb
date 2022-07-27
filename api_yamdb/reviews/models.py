e35e9a324fee8050854bdf16b7374ef3488aa256
Александр Морев <a.morev@kryptonite.ru>
2022-07-27 22:51:11 +0300

    deleted unused imports

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,9 +1,6 @@
-from django.contrib.auth import get_user_model
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 from django.contrib.auth.models import AbstractUser
-
-#  User = get_user_model()
 
 ROLES = (
     ('user','пользователь'),
