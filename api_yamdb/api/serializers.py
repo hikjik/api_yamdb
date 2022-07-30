a9fa852a65514b7e08541628bd88490ba2747a1b
Евгений Бузуев <ezjell@yandex.ru>
2022-07-30 15:32:53 +0300

    пустой комит

--- api_yamdb/api/serializers.py
+++ api_yamdb/api/serializers.py
@@ -3,6 +3,7 @@ from reviews.models import Category, Genre, Title
 
 
 class CategorySerializer(serializers.ModelSerializer):
+    
     class Meta:
         fields = ('__all__')
         model = Category
--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,11 +1,11 @@
+from api.permissions import IsAdminOrReadOnly
 from api.serializers import (CategorySerializer, GenreSerializer,
                              TitleSerializer)
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters, viewsets
 from rest_framework.pagination import LimitOffsetPagination
-from reviews.models import Category, Genre, Title
-from api.permissions import IsAdminOrReadOnly
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from reviews.models import Category, Genre, Title
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
