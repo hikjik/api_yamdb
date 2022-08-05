4908117d98fe41cad05509ad43ffebececc94328
Александр Морев <a.morev@kryptonite.ru>
2022-08-05 12:04:12 +0300

    Orgfnize urls in api

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,28 +1,8 @@
 from django.urls import include, path
-from rest_framework.routers import DefaultRouter
 
-from api.v1.views import (AuthViewSet, CategoryViewSet, CommentViewSet,
-                          GenreViewSet, ReviewViewSet, TitleViewSet,
-                          UserViewSet)
-
-v1_router = DefaultRouter()
-v1_router.register("auth", AuthViewSet)
-v1_router.register("users", UserViewSet)
-v1_router.register("categories", CategoryViewSet)
-v1_router.register("genres", GenreViewSet)
-v1_router.register("titles", TitleViewSet)
-v1_router.register(
-    r"titles/(?P<title_id>\d+)/reviews",
-    ReviewViewSet,
-    basename="reviews",
-)
-v1_router.register(
-    r"titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)/comments",
-    CommentViewSet,
-    basename="comments",
-)
+from .v1.urls import urlpatterns as api_v1
 
 
 urlpatterns = [
-    path("v1/", include(v1_router.urls)),
+    path("v1/", include(api_v1)),
 ]
